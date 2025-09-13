b65177ecb3558a9dd767bc97e4d898e2cc0472f1
Dheeraj-Kumar-8 <dheerajdasari88@gmail.com>
2025-09-13 20:04:28 +0530

    DrivingLicense_Eligibility.py

---
+++ DrivingLicense_Eligibility.py
@@ -0,0 +1,7 @@
+name = input("Enter Your Name: ")
+age = int(input("Enter Your Age :"))
+
+if(age >= 18):
+    print(name+" is eligible for driving a vehice.")
+else:
+    print(name+" is not eligible for driving a vehice.")
